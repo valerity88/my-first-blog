b840282ebc01a08b5fe8c4deecb90047552e5d38
valerity <valerity@inbox.ru>
2021-10-21 15:36:00 +0700

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length = 200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
-    publisher_date = models.DateTimeField(blank = True, null = True)
+    published_date = models.DateTimeField(blank = True, null = True)
 
     def publish(self):
-        self.publisher_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
